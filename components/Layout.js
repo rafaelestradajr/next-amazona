fac65a63a6381195066a0c89fa3333e7a285c68a
Rafael Estrada <rafaelestrada@comcast.net>
2021-12-17 20:41:57 -0800

    initial commit

---
+++ components/Layout.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Head from 'next/head';
+import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+
+export default function Layout({children}) {
+    return (
+        <div>
+            <Head>
+                <title>Next Amazona</title>
+
+            </Head>
+            <AppBar position = 'static'>
+                <Toolbar>
+                    <Typography>Amazona</Typography>
+                </Toolbar>
+
+            </AppBar>
+            <Container>
+                {children}
+            </Container>
+            <footer>
+                <Typography>
+                    All rights reserved. Next Amazona
+                </Typography>
+
+
+            </footer>
+        </div>
+    )
+}
