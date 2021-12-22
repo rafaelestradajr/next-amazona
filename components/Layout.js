a7437f61aaf74c33466927a4a39d8cc9ca902660
Rafael Estrada <rafaelestrada@comcast.net>
2021-12-21 20:26:26 -0800

    added middleware to backend

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import Head from 'next/head';
-import { AppBar, Container, CssBaseline, Link, ThemeProvider, Toolbar, Typography,createMuiTheme} from '@material-ui/core';
+import { AppBar, Container, CssBaseline, Link, ThemeProvider, Toolbar, Typography,createTheme,Switch} from '@material-ui/core';
 import useStyles from '../utils/styles';
 import NextLink from  'next/link';
+import {Store} from '../utils/Store';
+import Cookies from 'js-cookie';
 
 
 export default function Layout({title,description,children}) {
-    const theme = createMuiTheme({
+
+    const {state,dispatch} = useContext(Store);
+    const {darkMode} = state;
+    const theme = createTheme({
         typography: {
             h1:{
                 fontSize:'1.6rem',
@@ -20,7 +25,7 @@ export default function Layout({title,description,children}) {
             },
         },
         palette:{
-            type:'light',
+            type: darkMode? 'dark' :'light',
             primary:{
                 main:'#f0c000',
 
@@ -31,6 +36,11 @@ export default function Layout({title,description,children}) {
         },
     });
     const classes = useStyles();
+    const darkModeChangeHandler =() => {
+        dispatch({type:darkMode? 'DARK_MODE_OFF' : 'DARK_MODE_ON'});
+        const newDarkMode = !darkMode;
+        Cookies.set('darkMode', newDarkMode?'ON' : 'OFF');
+    }
     return (
         <div>
             <Head>
@@ -55,6 +65,7 @@ export default function Layout({title,description,children}) {
                     </NextLink>
                     <div className={classes.grow}></div>
                     <div>
+                        <Switch checked={darkMode} onChange={darkModeChangeHandler}></Switch>
                         <NextLink href='/cart' passHref>
                             <Link>Cart</Link>
                         </NextLink>
